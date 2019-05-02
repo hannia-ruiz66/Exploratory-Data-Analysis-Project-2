f4043b7f326fc19f1df8c9d63eb2d4d08c04af4a
hannia-ruiz66 <43188227+hannia-ruiz66@users.noreply.github.com>
2019-05-01 19:49:52 -0500

    Update and rename Question 5 to Question 5.R

--- Question 5.R
+++ Question 5.R
@@ -11,3 +11,6 @@ g+geom_bar(stat="identity",fill="grey",width=0.75) +
   guides(fill=FALSE) +
   labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
   labs(title=expression("PM"[2.5]*" Motor Vehicle Source Emissions in Baltimore from 1999-2008"))
+#Question 5
+#How have emissions from motor vehicle sources changed from 1999–2008 in Baltimore City?
+#Emissions from motor vehicle sources have dropped from 1999-2008 in Baltimore City!
