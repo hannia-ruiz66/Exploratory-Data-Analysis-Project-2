0d0e016816768096afb87364ea1e3e500ee07d1b
hannia-ruiz66 <43188227+hannia-ruiz66@users.noreply.github.com>
2019-05-01 19:45:31 -0500

    Update and rename Question 2 to Question 2.R

--- Question 2.R
+++ Question 2.R
@@ -9,3 +9,6 @@ barplot(
   ylab="PM2.5 Emissions (10^6 Tons)",
   main="Total PM2.5 Emissions From All Baltimore City Sources"
 )
+
+#Question 1#
+As observed the total emissions have decreased in the US from 1999 to 2008
