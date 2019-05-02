d97a966e360e1a121cf328b6495534d528594ee4
hannia-ruiz66 <43188227+hannia-ruiz66@users.noreply.github.com>
2019-05-01 19:49:03 -0500

    Update and rename Question 4 to Question 4.R

--- Question 4.R
+++ Question 4.R
@@ -6,11 +6,13 @@ combustionSCC <- SCC[coalCombustion,]$SCC
 combustionNEI <- NEI[NEI$SCC %in% combustionSCC,]
 
 library(ggplot2)
-
 ggp <- ggplot(combustionNEI,aes(factor(year),Emissions/10^5)) +
   geom_bar(stat="identity",fill="grey",width=0.75) +
   theme_bw() +  guides(fill=FALSE) +
   labs(x="year", y=expression("Total PM"[2.5]*" Emission (10^5 Tons)")) + 
   labs(title=expression("PM"[2.5]*" Coal Combustion Source Emissions Across US from 1999-2008"))
-
 print(ggp)
+
+#Question 4
+#Across the United States, how have emissions from coal combustion-related sources changed from 1999–2008?
+#Emissions from coal combustion related sources have decreased from 6 * 10^6 to below 4 * 10^6 from 1999-2008
